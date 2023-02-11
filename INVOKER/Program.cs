2e27d15c80cdaecf8e5ddace3a2dca27479f5c6d
enoreex <125078218+enoreex@users.noreply.github.com>
2023-02-11 15:29:22 +0100

    Добавьте файлы проекта.

---
+++ INVOKER/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+using INVOKER.src;
+
+namespace INVOKER
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class target = new Class();
+
+            Invoker.Log += Console.WriteLine;
+
+            Invoker.Invoke(target, "WriteTime"); //DateTime.Now
+
+            Invoker.Invoke(target, "WriteMsg", "Test!"); //Test!
+
+            Console.WriteLine(Invoker.Invoke(target, "Sum", 24, 43)); //67
+        }
+
+
+    }
+
+    internal class Class
+    {
+
+        public void WriteTime() => Console.WriteLine(DateTime.Now);
+
+        public void WriteMsg(string msg) => Console.WriteLine(msg);
+
+        public int Sum(int first, int second) { return first + second; }
+    }
+}
---
+++ INVOKER/src/Invoker.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+namespace INVOKER.src
+{
+    internal class Invoker
+    {
+        public static event Action<object>? Log;
+
+        public static dynamic Invoke(object? target, string? methodName, params object?[] args)
+        {
+            try
+            {
+                if(target is null)
+                    throw new ArgumentNullException(nameof(target));
+
+                if(methodName is null)
+                    throw new AbandonedMutexException(nameof(methodName));
+
+                Type type = target.GetType();
+                MethodInfo method = type.GetMethod(methodName)!;
+
+                if (method is null)
+                    throw new Exception($"[{methodName}] Method not found");
+
+                object result = method.Invoke(target, args)!;
+                return result!;
+            }
+            catch (Exception exc)
+            {
+                Log?.Invoke($"Invoker exception: {exc.Message}!");
+                return null!;
+            }
+        }
+    }
+}
